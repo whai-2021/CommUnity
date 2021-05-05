da330bc9002822eb5053cc1325025cb1e5513e45
ysabel-guiang <ysabelregino@yahoo.com>
2021-05-06 11:58:59 +1200

    passport routing complete

---
+++ server/routes/passport.js
@@ -0,0 +1,71 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const router = express.router()
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const initializePassport = require('./passport-config')
+initializePassport(passport)
+
+const users = require('../db/untils/users')
+
+router.use(flash())
+// might need to use true for resave and saveUnlimited
+router.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+router.use(passport.initialize())
+router.use(passport.session())
+
+router.get('/user', (req, res) => {
+  res.json(req.user)
+})
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) throw err
+    if (!user) res.json('No User Exists')
+    else {
+      req.logIn(user, err => {
+        if (err) throw err
+        res.json('Successfully Authenticated')
+        // console.log(req.user)
+      })
+    }
+  })(req, res, next)
+})
+
+// link db functions
+router.post('/register', (req, res) => {
+  const { firstName, lastName, username, password } = req.body
+
+  users.userExists(username)
+    .then(user => {
+      if (!user) {
+        // eslint-disable-next-line promise/no-nesting
+        users.createUser(firstName, lastName, username, password)
+          .then(() => {
+            res.json('user created')
+            return null
+          })
+          .catch(err => {
+            console.log(err.message)
+            return null
+          })
+        return null
+      } else {
+        res.json('username already exists')
+        return null
+      }
+    })
+    .catch(err => {
+      console.log(err.message)
+      return null
+    })
+})
---
+++ server/routes/passportConfig.js
@@ -0,0 +1,27 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const users = require('../db/untils/users')
+
+// link db functions
+export default function initialize (passport) {
+  passport.use(new LocalStrategy(async (username, password, done) => {
+    const user = users.userExists(username)
+
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' })
+    }
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Incorrect password.' })
+      }
+    } catch (error) {
+      return done(error)
+    }
+  }))
+  passport.serialzeUser((user, done) => done(null, user.id))
+  passport.deserialzeUser((id, done) => {
+    return done(null, users.getUserById(id))
+  })
+}
