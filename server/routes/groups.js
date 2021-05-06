16b540a03ddbe718eea3728bb1a0bf91d540fe09
Jatin-Puri <jatinpuri11@gmail.com>
2021-05-06 17:15:52 +1200

    create routes for user, group, posts

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -3,22 +3,140 @@ const router = express.Router()
 
 const db = require('../db/util/groups')
 
-// GET groups that user is apart of
-// PUT user joins group (Add user to a group)
-// GET members of a group
-// DELETE user leaves group
-// DELETE group
-//
-
-// GET all groups
+//GET all groups and get User groups
 router.get('/', (req,res) => {
-  db.getGroups()
-    .then(groups => {
-      res.json(groups)
+  const userId = Number(req.query.userId)
+
+  if (userId) {
+    db.getUsersGroups(userId)
+      .then((groups) => {
+        res.json(groups)
+      })
+      .catch(err => {
+        console.log(err.message)
+        res.sendStatus(500)
+      })
+  } else {
+    db.getGroups()
+      .then(groups => {
+        res.json(groups)
+      })
+      .catch(err => {
+        res.sendStatus(500)
+        console.log(err.message)
+      })
+  }
+})
+
+
+// GET specific group
+router.get('/:groupId', (req,res) => {
+  const groupId = Number(req.params.groupId)
+
+  db.getGroupById(groupId)
+    .then(group => {
+      res.json(group)
     })
     .catch(err => {
       res.sendStatus(500)
       console.log(err.message)
+    })
+})
+
+// POST group
+router.post('/', (req,res) => {
+  const { name, regionId } = req.body
+
+  db.addGroup({name, regionId})
+    .then((group) => {
+      res.json(group)
+    })
+    .catch(err => {
+      res.sendStatus(500)
+      console.log(err.message)
+    })
+})
+
+// DELETE group
+router.delete('/:groupId', (req, res) => {
+  const groupId = Number(req.params.groupId)
+
+  db.deleteGroup(groupId)
+    .then(() => {
+      console.log(groupId)
+      db.getGroupMembers(groupId)
+        .then((members) => {
+          const promises = members.map((member) =>
+            db.removeMemberFromGroup(member.id, groupId)
+          )
+          Promise.all(promises)
+            .then(() => {
+                res.sendStatus(200)
+              })
+              .catch(err => {
+                res.sendStatus(500)
+                console.log(err.message)
+              })
+        })
+        .catch(err => {
+          res.sendStatus(500)
+          console.log(err.message)
+        })
+    })
+    .catch(err => {
+      res.sendStatus(500)
+      console.log(err.message)
+    })
+})
+
+// PUT add a user to a group
+router.put('/:groupId/members', (req,res) => {
+  const groupId = Number(req.params.groupId)
+  const {userId} = req.body
+
+  db.addMemberToGroup(userId, groupId)
+    .then((group) => {
+      res.json(group)
+    })
+    .catch(err => {
+      res.sendStatus(500)
+      console.log(err.message)
+    })
+})
+
+// GET members of a group
+router.get('/:groupId/members', (req,res) => {
+  const groupId = Number(req.params.groupId)
+
+  db.getGroupMembers(groupId)
+    .then((members) => {
+      res.json(members)
+    })
+    .catch(err => {
+      console.log(err.message)
+      res.sendStatus(500)
+    })
+})
+
+//DELETE user from a group
+router.delete('/:id/members', (req, res) => {
+  const id = Number(req.params.id)
+  const {userId} = req.body
+
+  db.removeMemberFromGroup(userId, id)
+    .then(() => {
+      db.getGroupMembers(id)
+        .then(members => {
+          res.json(members)
+        })
+        .catch(err => {
+          console.log(err.message)
+          res.sendStatus(500)
+        })
+    })
+    .catch(err => {
+      console.log(err.message)
+      res.sendStatus(500)
     })
 })
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,13 +4,23 @@ const router = express.Router()
 const db = require('../db/util/users')
 
 // GET all users
-// DELETE user
-
 router.get('/', (req, res) => {
   db.getUsers()
     .then(users => {
       res.json(users)
       return null
+    })
+    .catch(e => {
+      res.status(500).send(e.message)
+    })
+})
+
+// DELETE user
+router.delete('/userId', (req, res) => {
+  const userId = req.params.id
+  db.deleteUser(userId)
+    .then(() => {
+      res.sendStatus(200)
     })
     .catch(e => {
       res.status(500).send(e.message)
