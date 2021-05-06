5fbfd647c4979ff79ada6622fa941a678e34a0a9
ysabel-guiang <ysabelregino@yahoo.com>
2021-05-07 10:13:31 +1200

    create pageLinks

---
+++ client/components/PageLinks.jsx
@@ -0,0 +1,9 @@
+import React from 'React'
+// component to be imported into every component that needs it
+function PageLinks () {
+  return (
+    <h1>PageLinks</h1>
+  )
+}
+
+export default PageLinks
