b7d6d379f689d85aebaa4a18d77677441e1dd879
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-06 14:40:32 +1200

    Created Login component

---
+++ client/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login () {
+  return (
+    <div>
+      <div>
+        <h2>Login to your account</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Login
