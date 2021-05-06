6c89008306b3455490a7dda6aa925280725a7bd6
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-06 14:42:12 +1200

    Changed Login to SignIn component

---
+++ client/components/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignIn () {
+  return (
+    <div className="">
+      <div>
+        <h2>Sign In to your account</h2>
+      </div>
+    </div>
+  )
+}
+
+export default SignIn
