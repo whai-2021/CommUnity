b38673b81ad8f43b2124eda6b7033d05dda7be00
Caleb Ion <calebionpersonal@gmail.com>
2021-05-07 11:31:01 +1200

    Add groups api client

---
+++ client/apis/groups.js
@@ -0,0 +1,15 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1/groups'
+
+export function getGroups () {
+  return request
+    .get(rootUrl)
+    .then(res => res.body)
+}
+
+export function getUsersGroups (userId) {
+  return request
+    .get(rootUrl + '?userId=' + userId)
+    .then(res => res.body)
+}
