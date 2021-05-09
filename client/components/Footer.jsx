7fd7a9ba602550f6345bbf47e53965ed9757e31d
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-09 22:14:55 +1200

    Started designing Pagelinks component

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer () {
   return (
-    <div>
+    <div className='flex items-center justify-center px-20 py-16 text-xs'>
       <p>Copyright &copy; 2021 Comm(unity)</p>
     </div>
   )
--- client/components/PageLinks.jsx
+++ client/components/PageLinks.jsx
@@ -1,24 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import Footer from './Footer'
+
 function PageLinks () {
   return (
-    <div className="container mx-auto sm">
-      <div className="grid grid-cols-4 divide-x divide-black divide-opacity-100 content-center">
-        <NavLink to='/newsfeed'><button className="bg-transparent hover:bg-gray-300 hover:text-gray-600 font-medium font-sans">
-            News Feed
-        </button></NavLink>
-        <NavLink to='/whatshappening'><button className="bg-transparent hover:bg-gray-300 hover:text-gray-600 font-medium font-sans">
-          Whats Happening
-        </button></NavLink>
-        <NavLink to='/information'><button className="bg-transparent hover:bg-gray-300 hover:text-gray-600 font-medium font-sans">
-          Information
-        </button></NavLink>
-        <NavLink to='/getsupport'><button className="bg-transparent hover:bg-gray-300 hover:text-gray-600 font-medium font-sans">
-          Get Support
-        </button></NavLink>
-
+    <div>
+      <div className='flex justify-between ml-12 mr-12 px-2 py-2'>
+        <NavLink to='/newsfeed'><button className=''>News Feed</button></NavLink>
+        <NavLink to='/whatshappening'><button className=''>Whats Happening</button></NavLink>
+        <NavLink to='/information'><button className=''>Information</button></NavLink>
+        <NavLink to='/getsupport'><button className=''>Get Support</button></NavLink>
       </div>
+      <Footer />
     </div>
   )
 }
