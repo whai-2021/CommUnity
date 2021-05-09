67b573f63d4adf9bc625214d52b8730d9ecb8d6d
ysabel-guiang <ysabelregino@yahoo.com>
2021-05-10 09:50:41 +1200

    fix seed files

--- server/db/seeds/Posts.js
+++ server/db/seeds/Posts.js
@@ -1,9 +1,9 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('posts').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('posts').insert([
         { id: 1, author_id: 1, group_id: 1, body: 'Warehouse Stationary is having a sale on back to school Products!', created_at: new Date(Date.now()) }
       ])
     })
