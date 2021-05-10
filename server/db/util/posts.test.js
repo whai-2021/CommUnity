71239113e681a785e5909651e8d9eea4b24a8a9c
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-10 15:42:37 +1200

    Passed getPosts under posts.test.js

---
+++ server/db/util/posts.test.js
@@ -0,0 +1,20 @@
+const db = require('./users')
+
+const knex = require('knex')
+const testConfig = require('../knexfile').test
+
+const { getPosts } = require('./posts')
+
+const testDb = knex(testConfig)
+
+beforeAll(() => testDb.migrate.latest())
+beforeEach(() => testDb.seed.run())
+
+test('can return all posts', () => {
+  expect.assertions(1)
+  return getPosts(testDb)
+    .then(posts => {
+      expect(posts).toHaveLength(1)
+      return null
+    })
+})
