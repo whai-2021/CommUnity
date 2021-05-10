a913aecd7daa801248e0e1e225b9765057efbbe4
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-11 11:37:59 +1200

    Created groups.test.js

--- server/routes/groups.test.js
+++ server/routes/groups.test.js
@@ -1,41 +1,41 @@
 import request from 'supertest'
 
 import
-{ getPosts } from '../db/util/posts'
+{ getGroups } from '../db/util/groups'
 
 import server from '../server'
 
-jest.mock('../db/util/posts', () => ({
-  getPosts: jest.fn()
+jest.mock('../db/util/groups', () => ({
+  getGroups: jest.fn()
 }))
 
-describe('GET /api/v1/posts', () => {
-  const fakePosts = [{
+describe('GET /api/v1/groups', () => {
+  const fakeGroups = [{
     id: 5,
     author_id: 10,
     group_id: 15,
     body: 'I am testing'
   }]
   beforeAll(() => {
-    getPosts.mockImplementation(() => {
-      return Promise.resolve(fakePosts)
+    getGroups.mockImplementation(() => {
+      return Promise.resolve(fakeGroups)
     })
   })
-  test('can GET posts', () => {
+  test('can GET groups', () => {
     expect.assertions(1)
     return request(server)
-      .get('/api/v1/posts')
+      .get('/api/v1/groups')
       .then(res => {
-        expect(res.body).toEqual(fakePosts)
+        expect(res.body).toEqual(fakeGroups)
         return null
       })
   })
 
   test('returns 500 if error', () => {
-    getPosts.mockImplementation(() => Promise.reject(new Error('Oopsie')))
+    getGroups.mockImplementation(() => Promise.reject(new Error('Oopsie')))
     expect.assertions(1)
     return request(server)
-      .get('/api/v1/posts')
+      .get('/api/v1/groups')
       .then(res => {
         expect(res.status).toEqual(500)
         return null
