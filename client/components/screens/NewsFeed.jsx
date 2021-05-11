7a9682ca9bd05f0ad0f3048c448f2f8baaacea4f
Jatin-Puri <jatinpuri11@gmail.com>
2021-05-11 17:02:14 +1200

    news showing

--- client/components/screens/NewsFeed.jsx
+++ client/components/screens/NewsFeed.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PageLinks from '../PageLinks'
 
+import { getNews } from '../../apis/newsapi'
+
 function NewsFeed () {
+  const [news, setNews] = useState([])
+
+  useEffect(() => {
+    getNews()
+      .then(res => {
+        console.log(res)
+        setNews(res.articles)
+        return null
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }, [])
   return (
     <>
-      {/* placeholder */}
       <PageLinks/>
+      <ul>
+        { news.map((article, index) => { return <li key={index}>{article.title}</li> }) }</ul>
     </>
   )
 }
