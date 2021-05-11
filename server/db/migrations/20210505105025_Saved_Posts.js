0424984edafe91ad4867cd439c8ae79de85e818c
ysabel-guiang <ysabelregino@yahoo.com>
2021-05-11 12:17:15 +1200

    stashing

--- server/db/migrations/20210505105025_Saved_Posts.js
+++ server/db/migrations/20210505105025_Saved_Posts.js
@@ -1,5 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('saved_posts', (table) => {
+    table.integer('post_id')
+    table.integer('user_id')
     table.primary(['user_id', 'post_id'])
   })
 }
