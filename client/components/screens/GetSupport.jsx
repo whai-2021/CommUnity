c2ced84eac2c393529b45edbe66ab285a9f44bd7
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-11 22:49:02 +1200

    Done getSupport has a new style

--- client/components/screens/GetSupport.jsx
+++ client/components/screens/GetSupport.jsx
@@ -6,9 +6,9 @@ function GetSupport () {
   return (
     <div>
       <PageLinks/>
-      <div className='flex justify-center items-center flex-col-reverse'>
-        <img width="400" src="./images/Logo(invisible).png" alt="Comm(unity) logo" />
-        <div className='mt-12'>
+      <div className='flex justify-center items-center flex-col-reverse lg:flex-row-reverse lg: justify-around mt-16'>
+        <img className='w-screen lg:max-w-xl' src="./images/Logo(invisible).png" alt="Comm(unity) logo" />
+        <div className='mt-12 text-center lg:text-left'>
           <h2 className='font-sans text-4xl'>New Place. New Experience.</h2>
           <p className='font-bold mt-5'>Join <em>the</em> Community.</p>
           <button className='inline-block bg-blue-400 px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wide mt-5'>Get Support</button>
