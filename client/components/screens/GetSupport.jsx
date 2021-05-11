c5d4fe57fcc88294a737b0ccbd02d061d7c2a75f
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-11 22:37:48 +1200

    Added a tag line and use the logo

--- client/components/screens/GetSupport.jsx
+++ client/components/screens/GetSupport.jsx
@@ -6,7 +6,14 @@ function GetSupport () {
   return (
     <div>
       <PageLinks/>
-      <img src="./images/Logo(invisible).png" alt="Comm(unity) logo" />
+      <div className='flex justify-center items-center flex-col-reverse'>
+        <img width="400" src="./images/Logo(invisible).png" alt="Comm(unity) logo" />
+        <div className='mt-12'>
+          <h2 className='font-sans text-4xl'>New Place. New Experience.</h2>
+          <p className='font-bold mt-5'>Join <em>the</em> Community.</p>
+          <button className='inline-block bg-blue-400 px-10 py-5 rounded-full shadow-lg uppercase text-lg tracking-wide mt-5'>Get Support</button>
+        </div>
+      </div>
       <Footer />
     </div>
   )
