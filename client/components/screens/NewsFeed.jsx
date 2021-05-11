e4987a21daa0b44dd54941652141b88e61e3eb7c
Jatin-Puri <jatinpuri11@gmail.com>
2021-05-12 00:01:24 +1200

    put articles in grid

--- client/components/screens/NewsFeed.jsx
+++ client/components/screens/NewsFeed.jsx
@@ -5,6 +5,20 @@ import { getNews } from '../../apis/newsapi'
 
 function NewsFeed () {
   const [news, setNews] = useState([])
+  const [fullArticle, setFullArticle] = useState(false)
+
+  function getFullArticle () {
+    return (
+      <div>
+        { news.map((article, index) => {
+          return <div className='py-2' key={index}>
+            <p>{article.content}</p>
+          </div>
+        }) }
+
+      </div>
+    )
+  }
 
   useEffect(() => {
     getNews()
@@ -20,8 +34,18 @@ function NewsFeed () {
   return (
     <>
       <PageLinks/>
-      <ul>
-        { news.map((article, index) => { return <li key={index}>{article.title}</li> }) }</ul>
+      <div className='w-screen'>
+        <h1 className='w-full py-10 m-4 text-center text-4xl font-bold text-gray-600'>New Zealand News</h1>
+        <div className='flex justify center grid gap-8 grid-cols-3 m-10'>
+          { news.map((article, index) => {
+            return <div className='py-2' key={index}>
+              <button onClick={() => setFullArticle(true)} className='text-blue-500 text-left'>{article.title}</button>
+              {fullArticle && getFullArticle()}
+              <p>{article.description}</p>
+            </div>
+          }) }
+        </div>
+      </div>
     </>
   )
 }
