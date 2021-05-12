7e04376ff1e44cd544dec3ec0ed86474f6646e5d
ysabel-guiang <ysabelregino@yahoo.com>
2021-05-12 18:24:03 +1200

    fix logo

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -7,7 +7,7 @@ function Nav ({ toggle }) {
       <header className='bg-white font-sans flex flex-col items-center'>
         <div className='w-full px-4 py-3'>
           <div className='flex items-center justify-between' onClick={toggle}>
-            <NavLink to='/'><img className='' width='150' src='/images/Community.png' /></NavLink>
+            <NavLink to='/'><img className='' width='150' src='/images/CommUnity.png' /></NavLink>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
           </div>
         </div>
